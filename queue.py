2e75224536ae9496fb40d217f1fadf81b4d820de
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-04-27 14:22:24 -0700

    Finished

---
+++ queue.py
@@ -0,0 +1,104 @@
+from linkedlist import LinkedList
+
+
+# Implement LinkedQueue below, then change the assignment at the bottom
+# to use this Queue implementation to verify it passes all tests
+class LinkedQueue(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this queue and enqueue the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.enqueue(item)
+
+    def __repr__(self):
+        """Return a string representation of this queue."""
+        return 'Queue({} items, front={})'.format(self.length(), self.front())
+
+    def is_empty(self):
+        """Return True if this queue is empty, or False otherwise."""
+        return self.length() == 0
+
+    def length(self):
+        """Return the number of items in this queue."""
+        return self.list.length()
+
+    def enqueue(self, item):
+        """Insert the given item at the back of this queue.
+        Running time: O(???) – Why? [TODO]"""
+        self.list.append(item)
+
+    def front(self):
+        """Return the item at the front of this queue without removing it,
+        or None if this queue is empty."""
+        if self.length() > 0:
+            return self.list.head.data
+
+        return None
+
+    def dequeue(self):
+        """Remove and return the item at the front of this queue,
+        or raise ValueError if this queue is empty.
+        Running time: O(???) – Why? [TODO]"""
+        if self.is_empty():
+            raise ValueError('The queue is currently empty')
+
+        item = self.list.get_at_index(0)
+        self.list.delete(item)
+
+        return item
+
+# Implement ArrayQueue below, then change the assignment at the bottom
+# to use this Queue implementation to verify it passes all tests
+class ArrayQueue(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this queue and enqueue the given items, if any."""
+        # Initialize a new list (dynamic array) to store the items
+        self.list = list()
+        if iterable is not None:
+            for item in iterable:
+                self.enqueue(item)
+
+    def __repr__(self):
+        """Return a string representation of this queue."""
+        return 'Queue({} items, front={})'.format(self.length(), self.front())
+
+    def is_empty(self):
+        """Return True if this queue is empty, or False otherwise."""
+        return self.length() == 0
+
+    def length(self):
+        """Return the number of items in this queue."""
+        return len(self.list)
+
+
+    def enqueue(self, item):
+        """Insert the given item at the back of this queue.
+        Running time: O(???) – Why? [TODO]"""
+        self.list.append(item)
+
+    def front(self):
+        """Return the item at the front of this queue without removing it,
+        or None if this queue is empty."""
+        if self.length() > 0:
+            return self.list[0]
+        
+        return None
+
+    def dequeue(self):
+        """Remove and return the item at the front of this queue,
+        or raise ValueError if this queue is empty.
+        Running time: O(???) – Why? [TODO]"""
+        if self.is_empty():
+            raise ValueError('The queue is currently empty')
+
+        return self.list.pop(0)
+
+
+# Implement LinkedQueue and ArrayQueue above, then change the assignment below
+# to use each of your Queue implementations to verify they each pass all tests
+Queue = LinkedQueue
+# Queue = ArrayQueue
---
+++ stack.py
@@ -0,0 +1,106 @@
+from linkedlist import LinkedList
+
+
+# Implement LinkedStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class LinkedStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        return self.list.is_empty()
+
+    def length(self):
+        """Return the number of items in this stack."""
+        return self.list.length()
+
+    def push(self, item):
+        """Insert the given item on the top of this stack.
+        Running time: O(???) – Why? [TODO]"""
+        self.list.append(item)
+
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        if self.is_empty():
+            return None
+
+        top = self.list.length() - 1
+        return self.list.get_at_index(top)
+
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(???) – Why? [TODO]"""
+        if self.is_empty():
+            raise ValueError('Stack is currently empty')
+
+        top = self.peek()
+
+        self.list.delete(top)
+
+        return top
+
+
+# Implement ArrayStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class ArrayStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new list (dynamic array) to store the items
+        self.list = list()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        return len(self.list) == 0
+
+    def length(self):
+        """Return the number of items in this stack."""
+        return len(self.list)
+
+    def push(self, item):
+        """Insert the given item on the top of this stack.
+        Running time: O(???) – Why? [TODO]"""
+        self.list.append(item)
+
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        if self.is_empty():
+            return None
+
+        return self.list[-1]
+
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(???) – Why? [TODO]"""
+        if self.is_empty():
+            raise ValueError('Stack is currently empty')
+
+        return self.list.pop()
+
+
+# Implement LinkedStack and ArrayStack above, then change the assignment below
+# to use each of your Stack implementations to verify they each pass all tests
+# Stack = LinkedStack
+Stack = ArrayStack
